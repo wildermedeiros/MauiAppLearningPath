42cd1fdf0c6d88cfa81e7626c049edc0bbc198b9
Wilder Medeiros <wildersmedeiros@gmail.com>
2023-11-29 17:20:40 -0300

    Improving entry behaviour with data validation and working on to convert number into words retrieved from firebase

--- MauiAppLearningPath/MainPage.xaml.cs
+++ MauiAppLearningPath/MainPage.xaml.cs
@@ -6,39 +6,139 @@ namespace MauiAppLearningPath
 {
     public partial class MainPage : ContentPage
     {
-        FirebaseClient firebaseClient = new FirebaseClient("https://mauifirebasesilicon-default-rtdb.firebaseio.com/");
+        readonly FirebaseClient FirebaseClient = new ("https://mauifirebasesilicon-default-rtdb.firebaseio.com/");
 
         public ObservableCollection<Note> Notes{ get; set; } = new ObservableCollection<Note>();
-
 
         public MainPage()
         {
             InitializeComponent();
 
             BindingContext = this;
+            DisplayDataFromServer();
+        }
 
-            var collection = firebaseClient
+        private void DisplayDataFromServer()
+        {
+            var collection = FirebaseClient
                 .Child("Notes")
                 .AsObservable<Note>()
-                .Subscribe((item) =>
+                .Subscribe(item =>
                 {
                     if (item.Object != null)
                     {
+                        string NoteDescription = item.Object.Description;
+                        if (int.TryParse(NoteDescription, out int StringToWorkOn))
+                        {
+                            NoteDescription = NumberToWords(StringToWorkOn);
+                        }
+
                         Notes.Add(new Note
                         {
-                            Description = item.Object.Description,
+                            Description = NoteDescription,
                         });
                     }
                 });
         }
 
-        private void OnSubmit(object sender, EventArgs e)
+        private void NoteDescriptionEntryOnCompleted(object sender, EventArgs e)
         {
-            firebaseClient.Child("Notes").PostAsync(new Note
+            if(!isDescriptionNullOrEmpty())
             {
-                Description = NoteDescriptionEntry.Text,
-            });
+                FirebaseClient.Child("Notes").PostAsync(new Note
+                {
+                    Description = NoteDescriptionEntry.Text,
+                });
+                NoteDescriptionEntry.Text = "";
+            }
+        }
+
+        private bool isDescriptionNullOrEmpty()
+        {
+            return string.IsNullOrEmpty(NoteDescriptionEntry.Text);
+        }
+
+        static string NumberToWords(int numero)
+        {
+            if (numero < 0 || numero > 1000000)
+            {
+                return "Número fora da faixa suportada.";
+            }
+
+            if (numero == 0)
+            {
+                return "Zero";
+            }
+
+            string[] unidades = { "", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove" };
+            string[] especiaisDezAteDezenove = { "Dez", "Onze", "Doze", "Treze", "Quatorze", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove" };
+            string[] dezenas = { "", "Dez", "Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa" };
+            string[] centenas = { "", "Cem", "Duzentos", "Trezentos", "Quatrocentos", "Quinhentos", "Seiscentos", "Setecentos", "Oitocentos", "Novecentos" };
+
+            if (numero <= 99)
+            {
+                return NumberUp99ToWords(numero);
+            }
+            else if (numero <= 999)
+            {
+                int centena = numero / 100;
+                int resto = numero % 100;
+                if (resto == 0)
+                {
+                    return centenas[centena];
+                }
+                else
+                {
+                    return centenas[centena] + " e " + NumberUp99ToWords(resto);
+                }
+            }
+            else
+            {
+                int milhar = numero / 1000;
+                int resto = numero % 1000;
+                if (resto == 0)
+                {
+                    return NumberToWords(milhar) + " Mil";
+                }
+                else
+                {
+                    return NumberToWords(milhar) + " Mil e " + NumberToWords(resto);
+                }
+            }
+        }
+
+        static string NumberUp99ToWords(int numero)
+        {
+            string[] unidades = { "", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove" };
+            string[] especiaisDezAteDezenove = { "Dez", "Onze", "Doze", "Treze", "Quatorze", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove" };
+            string[] dezenas = { "", "Dez", "Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa" };
+
+            if (numero == 0)
+            {
+                return "";
+            }
+            else if (numero < 10)
+            {
+                return unidades[numero];
+            }
+            else if (numero >= 10 && numero < 20)
+            {
+                return especiaisDezAteDezenove[numero - 10];
+            }
+            else
+            {
+                int unidade = numero % 10;
+                int dezena = numero / 10;
+
+                if (unidade == 0)
+                {
+                    return dezenas[dezena];
+                }
+                else
+                {
+                    return dezenas[dezena] + " e " + unidades[unidade];
+                }
+            }
         }
     }
-
 }
