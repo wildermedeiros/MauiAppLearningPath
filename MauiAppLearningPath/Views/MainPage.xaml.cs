02d8b440584aaf33a46db4e3b6bf447c15d3667e
Wilder Medeiros <wildersmedeiros@gmail.com>
2023-11-30 13:07:32 -0300

    Defining the behavior of each tab

--- MauiAppLearningPath/Views/MainPage.xaml.cs
+++ MauiAppLearningPath/Views/MainPage.xaml.cs
@@ -10,37 +10,9 @@ namespace MauiAppLearningPath.Views
     {
         readonly FirebaseClient FirebaseClient = new ("https://mauifirebasesilicon-default-rtdb.firebaseio.com/");
 
-        public ObservableCollection<Note> Notes{ get; set; } = new ObservableCollection<Note>();
-
         public MainPage()
         {
             InitializeComponent();
-
-            BindingContext = this;
-            DisplayDataFromServer();
-        }
-
-        private void DisplayDataFromServer()
-        {
-            var collection = FirebaseClient
-                .Child("Notes")
-                .AsObservable<Note>()
-                .Subscribe(item =>
-                {
-                    if (item.Object != null)
-                    {
-                        string NoteDescription = item.Object.Description;
-                        if (int.TryParse(NoteDescription, out int StringToWorkOn))
-                        {
-                            NoteDescription = NumberConverter.NumberToWords(StringToWorkOn);
-                        }
-
-                        Notes.Add(new Note
-                        {
-                            Description = NoteDescription,
-                        });
-                    }
-                });
         }
 
         private void NoteDescriptionEntryOnCompleted(object sender, EventArgs e)
--- MauiAppLearningPath/Views/RecordBook.xaml.cs
+++ MauiAppLearningPath/Views/RecordBook.xaml.cs
@@ -1,10 +1,45 @@
+using Firebase.Database;
+using MauiAppLearningPath.Models;
+using MauiAppLearningPath.Utils;
+using System.Collections.ObjectModel;
+
 namespace MauiAppLearningPath.Views
 {
 	public partial class RecordBook : ContentPage
 	{
-		public RecordBook()
+        readonly FirebaseClient FirebaseClient = new("https://mauifirebasesilicon-default-rtdb.firebaseio.com/");
+
+        public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+
+        public RecordBook()
 		{
 			InitializeComponent();
-		}
-	}
+
+            BindingContext = this;
+            DisplayDataFromServer();
+        }
+
+        private void DisplayDataFromServer()
+        {
+            var collection = FirebaseClient
+                .Child("Notes")
+                .AsObservable<Note>()
+                .Subscribe(item =>
+                {
+                    if (item.Object != null)
+                    {
+                        string NoteDescription = item.Object.Description;
+                        if (int.TryParse(NoteDescription, out int StringToWorkOn))
+                        {
+                            NoteDescription = NumberConverter.NumberToWords(StringToWorkOn);
+                        }
+
+                        Notes.Add(new Note
+                        {
+                            Description = NoteDescription,
+                        });
+                    }
+                });
+        }
+    }
 }
